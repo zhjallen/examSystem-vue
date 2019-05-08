5fa7b65ca489f51f9793f98873d2219e3992f6a2
zhjallen <1098414377@qq.com>
2019-05-08 23:18:43 +0800

    路由修改

--- src/router/test.ts
+++ src/router/test.ts
@@ -1,4 +1,4 @@
-import Layout from "../../modules/layout/views/index.vue";
+import Layout from "../modules/layout/views/index.vue";
 const testRouter = [{
     path: '/',
     component: Layout,
@@ -6,19 +6,19 @@ const testRouter = [{
         {
             path: 'admin/test/list',
             name: 'QuestionList',
-            component: resolve => require(['../../modules/admin/test/views/test.list.vue'], resolve),
+            component: resolve => require(['../modules/admin/test/views/test.list.vue'], resolve),
             meta: { title: '考试列表', icon: 'fdddfont el-icon-document' }
         },
         {
             path: 'admin/test/add',
             name: 'QuestionAdd',
-            component: resolve => require(['../../modules/admin/test/views/test.add.vue'], resolve),
+            component: resolve => require(['../modules/admin/test/views/test.add.vue'], resolve),
             meta: { title: '新增考试', icon: 'fdddfont el-icon-document' }
         },
         {
             path: 'admin/test/edit',
             name: 'QuestionEdit',
-            component: resolve => require(['../../modules/admin/test/views/test.edit.vue'], resolve),
+            component: resolve => require(['../modules/admin/test/views/test.edit.vue'], resolve),
             meta: { title: '修改考试', icon: 'fdddfont el-icon-document' }
         }
     ]
